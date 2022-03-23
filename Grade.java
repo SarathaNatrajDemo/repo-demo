ed4f0588526d8a72781a4227dca9abcf826f0ce6
SarathaNatraj <sarathanatrajan17@gmail.com>
2022-03-23 16:53:15 +0530

    changed the implementation Grades calculation

--- Grade.java
+++ Grade.java
@@ -1,18 +1,18 @@
 
 public class Grade {
-   public static char getLetterGrade(int mark) {
+   public static String getLetterGrade(int mark) {
       // assume that mark is between 0 and 100 (inclusive)
       assert (mark >= 0 && mark <= 100) : "mark is out-of-range: " + mark;
       if (mark >= 75) {
-         return 'A'; 
+         return "Very good"; 
       } else if (mark >= 60) {
-         return 'B';
+         return "Good";
       } else if (mark > 50) {   // an logical error here
-         return 'C';
+         return "Average";
       } else if(mark > 40){
-         return 'F';
+         return "Below Average";
       }else{
-	return 'U';
+    	  return "Unclassified";
       }
    }
 }
